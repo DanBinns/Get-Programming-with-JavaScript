86fd6118f03de1598206a91e5f3754fa7413d1dd
Binns <daniel.binns@sap.com>
2018-12-26 18:21:25 +0000

    Recipes.js updated

--- Recipes.js
+++ Recipes.js
@@ -1,25 +1,27 @@
+showRecipe = function () {
+  console.log ("Recipe Name: " + recipe.name);
+  console.log("Ingredients: " + recipe.ingredients);
+  console.log("Seasoning: " + recipe.seasoning);
+  console.log("Accompaniments: " + recipe.accompaniments);
+  console.log("*********************");
+};
+
 var recipe;
 
 var recipe1 = {
   name: "Red Spiced Chicken Pasta",
   ingredients: "Chicken, bacon, sun dried tomatoes, garlic, cream, pasta",
-  seasoning: "Salt, pepper, parmasan",
+  seasoning: "Salt, pepper",
+  accompaniments: "Parmasan, Garlic bread",
 };
 
 var recipe2 = {
   name: "Carbonara",
   ingredients: "Pancette, egg, pasta",
-  seasoning: "Salt, pepper, parmasan",
+  seasoning: "Salt, pepper",
+  accompaniments: "Parmasan, Garlic bread",
 }
 
-showRecipe = function () {
-  console.log ("Recipe Name: " + recipe.name);
-  console.log("Ingredients: " + recipe.ingredients);
-  console.log("Seasoning: " + recipe.seasoning);
-  console.log("*********************");
-};
-
 recipe = recipe1; showRecipe();
-
 
 recipe = recipe2; showRecipe();
