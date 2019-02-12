8aceb8b41e3a13c5955c7a41df16b0774d36a7ec
DanBinns <dbinns@live.co.uk>
2019-02-12 20:27:57 +0000

    5.1 created

---
+++ 5.1 Relying on a variable outside the function.js
@@ -0,0 +1,18 @@
+//Create variables
+
+var message;
+var showMessage;
+
+//Set message variable to the defined text
+
+message = "Hello World!";
+
+//Function using the message variable
+
+showMessage = function () {
+	console.log(message);
+};
+
+//Invoke the function
+
+showMessage();
