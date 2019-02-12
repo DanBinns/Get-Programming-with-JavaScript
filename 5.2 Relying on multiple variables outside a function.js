0ba6c48cd3db1ad7261c24ce9e9330d898ff8328
DanBinns <dbinns@live.co.uk>
2019-02-12 20:47:17 +0000

    5.2 and 5.3 complete

---
+++ 5.2 Relying on multiple variables outside a function.js
@@ -0,0 +1,11 @@
+var message1;
+var message2;
+var showMessage;
+
+message1 = "Hello";
+message2 = "World!"
+showMessage = function () {
+	console.log(message1 + " " + message2);
+};
+
+showMessage();
---
+++ 5.3 Passing information to a function.js
@@ -0,0 +1,8 @@
+var showMessage;
+
+showMessage = function (message) {
+	console.log("The message is: " + message + ":)" + '\n');
+};
+
+showMessage("It's full of stars!");
+showMessage("Hello World!");
